8cd0a3755131937fa91ebb000907bfd7ee09b5aa
yamsiu <mar6861214@gmail.com>
2019-02-17 00:41:40 +0800

    stateless component寫法

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const app = props => {
+  const [personsState, setPersonsState] = useState({
+    person: [
+      { name: "Messie", age: 38 },
+      { name: "Wang", age: 36 },
+      { name: "Nadal", age: 33 }
+    ]
+  });
 
-export default App;
+  const [otherState, setOtherState] = useState('something')
+
+  console.log(personsState, otherState)
+
+  const switchNameHandler = () => {
+    // console.log('click');
+    // state和setState只有component才有
+    setPersonsState({
+      person: [
+        { name: "Brady", age: 38 },
+        { name: "Wang", age: 36 },
+        { name: "Nadal", age: 39 }
+      ]
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.person[0].name} age={personsState.person[0].age} />
+      <Person name={personsState.person[1].name} age={personsState.person[1].age}>
+        Job: Professional baseball player
+      </Person>
+      <Person name={personsState.person[2].name} age={personsState.person[2].age} />
+    </div>
+  );
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!' ))
+};
+
+export default app;
+
+
