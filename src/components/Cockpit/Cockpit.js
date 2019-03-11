5689bc7dee7b21d24a4ccad8b111595b26a2e609
yamsiu <mar6861214@gmail.com>
2019-03-11 23:49:57 +0800

    React creatRef

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,14 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 import classes from "./Cockpit.css";
 
 const cockpit = props => {
 
+  const toggleBtnRef = useRef(null); //指定變數 toggleBtnRef 使用 useRef() 來取得DOM
+
   useEffect(() => {
     console.log("Cockpit.js userEffect...");
-    setTimeout(() => {
-      alert("save data to cloud");
-    }, 1000);
+    // setTimeout(() => {
+    //   alert("save data to cloud");
+    // }, 1000);
+    toggleBtnRef.current.click(); // 一定要在 useEffect() 內呼叫 toggleBtnRef，因此時 DOM 已建立完成才能取得。
     return () => {
       console.log("Cockpit.js cleanup work in useEffect...");
     };
@@ -39,7 +42,7 @@ const cockpit = props => {
     <div className={classes.Cockpit}>
       <h1>{props.title}</h1>
       <p className={assignedClasses.join(" ")}>This is really working!</p>
-      <button className={btnClass} onClick={props.clicked}>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle persons
       </button>
     </div>
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import Proptypes from 'prop-types';
 
 import Aux from "../../../hoc/Auxiliary";
 import withClass from "../../../hoc/withClass"
 import classes from "./Person.css";
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log("person.js render...");
     return (
@@ -15,12 +25,20 @@ class Person extends Component {
         <p>{this.props.children}</p>
         <input
           type="text"
+          ref={this.inputElementRef}
           onChange={this.props.changed}
           value={this.props.name}
         />
       </Aux>
     );
   }
+}
+
+Person.propTypes = {
+  click: Proptypes.func,
+  name: Proptypes.string,
+  age: Proptypes.number,
+  changed: Proptypes.func
 }
 
 export default withClass(Person, classes.Person);
